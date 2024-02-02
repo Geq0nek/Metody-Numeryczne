7afd09d3d91107932b87e957e68fe47eb1c30490
Geq0nek <93310012+Geq0nek@users.noreply.github.com>
2024-02-02 16:37:27 +0100

    Add files via upload

---
+++ laboratorium-5-Geq0nek/homework.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy as sp
+from scipy import linalg
+from  datetime import datetime
+import pickle
+
+from typing import Union, List, Tuple
+
+
+def spare_matrix_Abt(m: int,n: int):
+    """Funkcja tworząca zestaw składający się z macierzy A (m,n), wektora b (m,)  i pomocniczego wektora t (m,) zawierających losowe wartości
+    Parameters:
+    m(int): ilość wierszy macierzy A
+    n(int): ilość kolumn macierzy A
+    Results:
+    (np.ndarray, np.ndarray): macierz o rozmiarze (m,n) i wektorem (m,)
+                Jeżeli dane wejściowe niepoprawne funkcja zwraca None
+    """
+    if isinstance(m, int) and isinstance(n, int):
+        t = np.transpose(np.linspace(0, 1, m))
+        b = np.cos(4*t)
+        A = np.fliplr(np.vander(t, n))
+        return A, b
+    else:
+        return None
+
---
+++ laboratorium-5-Geq0nek/main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import scipy as sp
+from scipy import linalg
+from  datetime import datetime
+import pickle
+
+from typing import Union, List, Tuple
+
+
+def square_from_rectan(A: np.ndarray, b: np.ndarray):
+    """Funkcja przekształcająca układ równań z prostokątną macierzą współczynników na kwadratowy układ równań. Funkcja ma zwrócić nową macierz współczynników  i nowy wektor współczynników
+    Parameters:
+      A: macierz A (m,n) zawierająca współczynniki równania
+      b: wektor b (m,) zawierający współczynniki po prawej stronie równania
+    Results:
+    (np.ndarray, np.ndarray): macierz o rozmiarze (n,n) i wektorem (n,)
+             Jeżeli dane wejściowe niepoprawne funkcja zwraca None
+     """
+    if isinstance(A, np.ndarray) and isinstance(b, np.ndarray):
+      return np.dot(np.transpose(A), A), np.dot(np.transpose(A), b)
+    else:
+        return None
+
+
+
+def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray):
+    """Funkcja obliczająca normę residuum dla równania postaci:
+    Ax = b
+
+      Parameters:
+      A: macierz A (m,n) zawierająca współczynniki równania
+      x: wektor x (n,) zawierający rozwiązania równania
+      b: wektor b (m,) zawierający współczynniki po prawej stronie równania
+
+      Results:
+      (float)- wartość normy residuom dla podanych parametrów
+      """
+    if all(isinstance(t, np.ndarray) for t in [A, b, x]):
+      return np.linalg.norm(b - np.dot(A, np.transpose(x)))
+    
+       
